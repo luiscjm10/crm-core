4c465b939057cc2000d4c224163a10ef3b0d4b05
luis jimenez <jimenezmangaluiscarlos@gmail.com>
2026-06-24 00:29:18 -0500

    feat: redirigir a la vista de inicio de sesión en lugar de la página principal

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
